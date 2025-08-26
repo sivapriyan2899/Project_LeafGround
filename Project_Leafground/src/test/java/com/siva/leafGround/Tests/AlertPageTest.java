9c7b3a1c2be3863d7c0e1c0cc589e2eb9471b191
sivapriyan2899 <sivapriyan2899@gmail.com>
2025-08-26 19:56:16 +0530

    added extent report_testng xml file

--- Project_Leafground/src/test/java/com/siva/leafGround/Tests/AlertPageTest.java
+++ Project_Leafground/src/test/java/com/siva/leafGround/Tests/AlertPageTest.java
@@ -7,10 +7,16 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentEmailReporter;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.siva.leafGround.Pages.AlertPage;
 import com.siva.leafGround.Pages.InitializeBrowser;
 
@@ -19,6 +25,10 @@ public class AlertPageTest {
 
 	public  WebDriver driver; // declared WebDriver as instance variable, so one browser will get open and run all test case.
 	public  AlertPage alertPage; // declared to initialize elements in AlertPage.class
+	public ExtentReports extentReport;
+	public ExtentSparkReporter sparkReport;
+	ExtentEmailReporter emailReport;
+	public ExtentTest testCase;
 
 	// Helper method - switch to alert
 	public Alert switchToAlert(WebDriver x) {
@@ -44,10 +54,21 @@ public class AlertPageTest {
 
 	@BeforeTest
 	public  WebDriver navigateToAlertPage() {
+		extentReport = new ExtentReports();
+		sparkReport = new ExtentSparkReporter("sReport.html");
+		extentReport.attachReporter(sparkReport);
+		
+		
 		driver = InitializeBrowser.chromeBrowser();
 		String url ="https://leafground.com/alert.xhtml;jsessionid=node0qo3nh9g1hog612ch2drsbxu353858683.node0";
 		driver.get(url);
 		return driver;
+	}
+	
+	@AfterTest
+	public void closeBrowser() {
+		driver.quit();
+		extentReport.flush();
 	}
 	
 	@BeforeMethod
@@ -62,6 +83,8 @@ public class AlertPageTest {
 
 	@Test (priority = 0)
 	public void testCase01_1_alertSimpleDialog() {
+		testCase = extentReport.createTest("Alert_SimpleDialog_accept");
+		testCase.log(Status.INFO, "hii");
 		
 		alertPage.alertSimpleDialog.click();
 		switchToAlert(driver);
@@ -72,7 +95,7 @@ public class AlertPageTest {
 
 	@Test (priority = 1)
 	public void testCase01_2_alertSimpleDialog() {
-
+		testCase = extentReport.createTest("Alert_SimpleDialog_dismiss");
 
 		alertPage.alertSimpleDialog.click();
 		switchToAlert(driver);
@@ -83,7 +106,8 @@ public class AlertPageTest {
 	
 	@Test (priority = 2)
 	public void testCase01_3_alertSimpleDialog() {
-
+		testCase = extentReport.createTest("Alert_SimpleDialog_getAlertText");
+		
 		alertPage.alertSimpleDialog.click();
 		switchToAlert(driver);
 		Alert alert = webdriverWait();
@@ -95,7 +119,7 @@ public class AlertPageTest {
 
 	@Test (priority = 3)
 	public  void testCase02_1_alertPromptDialog() {
-		
+		testCase = extentReport.createTest("Alert_promptDialog_accept");
 		alertPage.alertPromptDialog.click();
 		
 		Alert alert = webdriverWait();
@@ -108,7 +132,7 @@ public class AlertPageTest {
 	
 	@Test(priority = 4)
 	public void testCase02_2_alertPromptDialog() {
-		
+		testCase = extentReport.createTest("Alert_promptDialog_getAlertText");
 		alertPage.alertPromptDialog.click();
 		
 		Alert alert = webdriverWait();
@@ -121,7 +145,7 @@ public class AlertPageTest {
 
 	@Test (priority = 5)
 	public  void testCase03_1_alertConfirmationDialog() {
-
+		testCase = extentReport.createTest("Alert_ConfirmationDialog_accept");
 		
 		alertPage.alertConfirmDialog.click();
 		Alert alertHandller = switchToAlert(driver);
@@ -131,6 +155,7 @@ public class AlertPageTest {
 
 	@Test (priority = 6)
 	public void testCase03_2_alertConfirmationDialog()  {
+		testCase = extentReport.createTest("Alert_ConfirmationDialog_verifyText");
 		
 		alertPage.alertConfirmDialog.click();
 		switchToAlert(driver);
